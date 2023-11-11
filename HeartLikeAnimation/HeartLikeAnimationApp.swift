b012be66ea56e7a02067d6a96614947f04659c60
githubuiux <uiuxista@gmail.com>
2023-11-11 23:06:15 +0300

    Initial Commit

---
+++ HeartLikeAnimation/HeartLikeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartLikeAnimationApp.swift
+//  HeartLikeAnimation
+//
+//  Created by Levent on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeartLikeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
